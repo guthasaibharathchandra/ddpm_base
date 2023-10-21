e90b97eb330025930c87d182e408ba083397c8aa
guthasaibharathchandra <guthasaibharath06@gmail.com>
2023-10-21 12:24:44 +0200

    updated base functions + improved speed up + added batched inference

--- lib.py
+++ lib.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
-
+#torch.set_default_dtype(torch.float32)
+# The numbering of timesteps starts from 1 to T and for index 0, alpha=1, beta=0
 def forward_diffusion(x, t_curr, t_next, schedule): #only for visualization (works with numpy inputs)
     
     #INPUT ASSERTIONS: BEGIN
@@ -46,9 +47,9 @@ def reverse_diffusion(ddpm_model, x, t_curr, schedule, cuda=True, onlymean=False
     assert (t_curr > 0) and (t_curr <= T)
     #INPUT ASSERTIONS: END
 
-    #ALGORITHM: BEGIN
-    x_inp = torch.Tensor([x])
-    t_curr_inp = torch.Tensor([t_curr])
+    #ALGORITHM: BEGIN  
+    x_inp = torch.from_numpy(x)
+    t_curr_inp = t_curr * torch.ones(x_inp.shape[0], dtype=torch.float32)
     device = 'cuda' if cuda else 'cpu'
     device = torch.device(device)
     ddpm_model.to_device(device) # model is an object of DDPM class below
@@ -56,14 +57,15 @@ def reverse_diffusion(ddpm_model, x, t_curr, schedule, cuda=True, onlymean=False
     t_curr_inp = t_curr_inp.to(device)
     with torch.no_grad():
         noise_pred = ddpm_model.infer(x_inp, t_curr_inp)
-        noise_pred = noise_pred.cpu().numpy()[0]
-    assert noise_pred.shape == x.shape
+        noise_pred = noise_pred.cpu().numpy()
+    assert noise_pred.shape == x.shape and noise_pred.dtype == x.dtype
     mean = (1.0/np.sqrt(1.0-beta[t_curr])) * ( x - ( (beta[t_curr]/np.sqrt(1-alpha[t_curr])) * noise_pred ) )
+    assert mean.dtype == x.dtype
     rstd = np.sqrt(rvar[t_curr])
     if onlymean:
         return mean
     
-    return mean + (rstd*np.random.normal(loc=0.0, scale=1.0, size=tuple(x.shape)))
+    return mean + np.asarray((rstd*np.random.normal(loc=0.0, scale=1.0, size=tuple(x.shape))),dtype=np.float32)
     #ALGORITHM: END
 
 def make_schedule(scheme, rvar, start_beta=0, end_beta=0.99, T=500):
@@ -76,88 +78,18 @@ def make_schedule(scheme, rvar, start_beta=0, end_beta=0.99, T=500):
     assert rvar=='beta' or rvar=='fvar'
     assert end_beta >= start_beta
     #INPUT ASSERTIONS: END
-    """
-    if scheme == 'linear':
-        
-        ans = 1.0
-        alpha = [1.0,]
-        beta = np.arange(start=0,stop=1+1e-9,step=1.0/T)
-        assert (len(beta) == T+1) and (beta[0] == 0)
-        for i in range(1,len(beta)):
-            ans *= 1.0-beta[i]
-            alpha.append(ans)
-        alpha = np.array(alpha)
 
-    elif scheme == 'exponential':
-        
-        ans = 1.0
-        alpha = [1.0,]
-        beta = np.arange(start=-4,stop=0+1e-9,step=4.0/T)
-        beta = np.exp(beta)
-        beta[0] = 0
-        assert (len(beta) == T+1) and (beta[0] == 0)
-        for i in range(1,len(beta)):
-            ans *= 1.0-beta[i]
-            alpha.append(ans)
-        alpha = np.array(alpha)
-
-    elif scheme == 'decay':
-        ans = 1.0
-        alpha = [1.0,]
-        beta = [0.0] * (T+1)
-        beta[T] = 1.0
-        #step = 0.9
-        step = np.exp(-6/T)
-        assert (len(beta) == T+1) and (beta[T] == 1.0)
-        for i in range(1,len(beta)):
-            beta[T-i] = beta[T-i+1]*step
-        beta[0] = 0
-        for i in range(1,len(beta)):
-            ans *= 1.0-beta[i]
-            alpha.append(ans)
-        alpha = np.array(alpha)
-    
-    elif scheme == 'inverse':
-        ans = 1.0
-        alpha = [1.0,]
-        beta = [0,] #np.arange(start=0,stop=1+1e-9,step=1.0/T)
-        for t in range(1,T+1):
-            beta.append(1.0/(T+1-t))
-        assert (len(beta) == T+1) and (beta[0] == 0)
-        for i in range(1,len(beta)):
-            ans *= 1.0-beta[i]
-            alpha.append(ans)
-        alpha = np.array(alpha)
-        #raise NotImplementedError
-    """
-    
     if scheme == 'linear':
         ans = 1.0
         alpha = [1.0,]
-        eps = 1e-6
-        #beta = [0,] #np.arange(start=0,stop=1+1e-9,step=1.0/T)
-        beta = [0,] + np.arange(start=start_beta,stop=end_beta+eps,step=(end_beta - start_beta)/(T-1)).tolist()
-        beta = np.array(beta)
+        eps = 1e-7
+        beta = [0,] + np.linspace(start=start_beta, stop=end_beta, endpoint=True, num=T).tolist()
+        beta = np.array(beta, dtype=np.float32)
         assert (len(beta) == T+1) and (beta[0] == 0) and (beta[T] - end_beta < eps) and (beta[1] - start_beta < eps)
         for i in range(1,len(beta)):
                 ans *= 1.0-beta[i]
                 alpha.append(ans)
-        alpha = np.array(alpha)
-
-    elif scheme == 'decay':
-        ans = 1.0
-        alpha = [1.0,]
-        beta = [0.0] * (T+1)
-        beta[T] = 0.05
-        step = np.exp(-5/T)
-        assert (len(beta) == T+1) and (beta[T] == 0.05)
-        for i in range(1,len(beta)):
-            beta[T-i] = beta[T-i+1]*step
-        beta[0] = 0
-        for i in range(1,len(beta)):
-            ans *= 1.0-beta[i]
-            alpha.append(ans)
-        alpha = np.array(alpha) 
+        alpha = np.array(alpha, dtype=np.float32)
 
     else:
         raise NotImplementedError
@@ -171,7 +103,7 @@ def make_schedule(scheme, rvar, start_beta=0, end_beta=0.99, T=500):
         rvar = [0.0]
         for i in range(1,len(beta)):
             rvar.append( ((1-alpha[i-1])*beta[i])/(1-alpha[i]) )
-        rvar = np.array(rvar)     
+        rvar = np.array(rvar, dtype=np.float32)     
 
     else:
         raise NotImplementedError
@@ -213,30 +145,26 @@ class DDPM():
         return torch.nn.MSELoss(reduction='mean')
         
     
-    def __sample_std_gaussian_noise(self, X):
+    def __sample_std_gaussian_noise(self, X): # X is already supposed to be on self.device
 
-        return np.random.normal(loc=0.0, scale=1.0, size=tuple(X.shape))
+        return torch.randn(*X.shape, device=self.device, dtype=torch.float32)
     
-    
-    def __generate_temporal_noise(self, X0):
+    def __generate_temporal_noise(self, X0): # X0 here is supposed to be already on self.device
 
         num_samples = X0.shape[0]
         
         #SAMPLE T
         T_curr = np.random.choice(np.arange(start=1, stop=1+self.T, step=1), size=num_samples, replace=True)    
         Alpha_T_curr = self.alpha[T_curr]
-        p = torch.from_numpy(np.sqrt(Alpha_T_curr).reshape(-1, *([1]*(len(X0.shape)-1)) ))
-        q = torch.from_numpy(np.sqrt(1.0-Alpha_T_curr).reshape(-1, *([1]*(len(X0.shape)-1)) ))
+        p = torch.tensor(np.sqrt(Alpha_T_curr).reshape(-1, *([1]*(len(X0.shape)-1)) ), dtype=torch.float32).to(self.device)
+        q = torch.tensor(np.sqrt(1.0-Alpha_T_curr).reshape(-1, *([1]*(len(X0.shape)-1)) ), dtype=torch.float32).to(self.device)
         
         #GENERATE X_T
         Noise_std_gaussian = self.__sample_std_gaussian_noise(X0)
-        X_T_curr = (p*X0.cpu().numpy()) + (q*Noise_std_gaussian)
-
-        X_T_curr = np.asarray(X_T_curr, dtype=np.float32)
-        T_curr = np.asarray(T_curr, dtype=np.float32)
-        Noise_std_gaussian = np.asarray(Noise_std_gaussian, dtype=np.float32)
-
-        return torch.Tensor(X_T_curr), torch.Tensor(T_curr), torch.Tensor(Noise_std_gaussian)
+        X_T_curr = (p*X0) + (q*Noise_std_gaussian)
+        T_curr = torch.from_numpy(np.asarray(T_curr, dtype=np.float32)).to(self.device)
+        
+        return X_T_curr, T_curr, Noise_std_gaussian
 
     def to_device(self, device):
         self.device = device
@@ -247,14 +175,17 @@ class DDPM():
         self.model.eval()
         X_T_curr, T_curr = X_T_curr.to(self.device), T_curr.to(self.device)
         Noise_std_gaussian_pred = self.model(X_T_curr, T_curr)
+        
         return Noise_std_gaussian_pred
 
     
     def run_step(self, X0): #only for training/validation run
+        
         self.model.train()
+        X0 = X0.to(self.device)
         X_T_curr, T_curr, Noise_std_gaussian = self.__generate_temporal_noise(X0)
-        X_T_curr, T_curr, Noise_std_gaussian = X_T_curr.to(self.device), T_curr.to(self.device), Noise_std_gaussian.to(self.device)
         Noise_std_gaussian_pred = self.model(X_T_curr, T_curr)
+        
         return self.loss(Noise_std_gaussian_pred, Noise_std_gaussian)
 
 
